b7ed9a477bf8415e03424e39001ce66908f79286
juniorCoderKrithika <72335062+juniorCoderKrithika@users.noreply.github.com>
2022-05-31 17:25:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(640,480);
+canvas.position(450,100);
+video=createCapture(VIDEO);
+video.hide();
+tint_color="";
+}
+function draw(){
+image(video,0,0,640,480);
+tint(tint_color);
+}
+function filter_tint(){
+    tint_color=document.getElementById("color_name").value;
+}
+function take_snapshot(){
+    save("download.png");
+}
